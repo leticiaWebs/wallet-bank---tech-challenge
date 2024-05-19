e7dfa4bb53f05121c2d9aa494d275d47e4f01e3a
leticiaWebs <leticiaaod@gmail.com>
2024-05-19 16:52:02 -0300

    Backend init

---
+++ wbcontadigital/src/test/java/com/walletbank/wbcontadigital/WbcontadigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walletbank.wbcontadigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WbcontadigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
